c76724028a4fb5771f5ca8228493dc26fbc062f4
Ioan Velev <ioanvelev46@gmail.com>
2025-10-23 19:14:11 +0300

    Update variable type assignments explicitly

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var investmentAmmount = 1000
+	var investmentAmmount float64 = 1000
 	var expectedReturnRate = 5.5
-	var years = 10
+	var years float64 = 10
 
-	var futureValue = float64(investmentAmmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	var futureValue = investmentAmmount * math.Pow(1+expectedReturnRate/100, years)
 
 	fmt.Println(futureValue)
 }
