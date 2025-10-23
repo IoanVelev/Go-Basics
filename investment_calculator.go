81b5d4d78569f63c04e8dd92b4d42ecfe43614a2
Ioan Velev <ioanvelev46@gmail.com>
2025-10-23 19:31:43 +0300

    Calculate final return with inflation rate

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmmount float64 = 1000
 	expectedReturnRate := 5.5
 	var years float64 = 10
 
 	futureValue := investmentAmmount * math.Pow(1+expectedReturnRate/100, years)
+	futureFinalValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureFinalValue)
 }
