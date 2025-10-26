817e9fba952a84879e887020e3295d60ae822d89
Ioan Velev <ioanvelev46@gmail.com>
2025-10-26 17:37:59 +0200

    Create formatted strings

--- investment_calculator.go
+++ investment_calculator.go
@@ -23,6 +23,11 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureFinalValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Printf("Future value: %.0f\n", futureValue)
-	fmt.Printf("Future final value (adjusted for inflation): %.0f", futureFinalValue)
+	formatedFV := fmt.Sprintf("Future value: %.0f\n", futureValue)
+	formatedFFV := fmt.Sprintf("Future final value (adjusted for inflation): %.0f\n", futureFinalValue)
+
+	// fmt.Printf("Future value: %.0f\n", futureValue)
+	// fmt.Printf("Future final value (adjusted for inflation): %.0f", futureFinalValue)
+
+	fmt.Print(formatedFV, formatedFFV)
 }
