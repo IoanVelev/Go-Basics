bcf235621f62363d595c8de102cd9ee23e5dcf9c
Ioan Velev <ioanvelev46@gmail.com>
2025-10-26 17:26:28 +0200

    Format Floats in Strings

--- investment_calculator.go
+++ investment_calculator.go
@@ -23,6 +23,6 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureFinalValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Printf("Future value: %v\n", futureValue)
-	fmt.Printf("Future final value (adjusted for inflation): %v", futureFinalValue)
+	fmt.Printf("Future value: %.0f\n", futureValue)
+	fmt.Printf("Future final value (adjusted for inflation): %.0f", futureFinalValue)
 }
