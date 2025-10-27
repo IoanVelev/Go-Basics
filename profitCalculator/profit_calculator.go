b34b82dc291cde6f5b8a25491493b4c36470e4a8
Ioan Velev <ioanvelev46@gmail.com>
2025-10-27 20:55:52 +0200

    Rename and update getUserInput function

--- profitCalculator/profit_calculator.go
+++ profitCalculator/profit_calculator.go
@@ -4,35 +4,37 @@ import (
 	"fmt"
 )
 
-var taxRate float64
-
 func main() {
 	var revenue float64
 	var expenses float64
+	var taxRate float64
 
 	// fmt.Print("Enter a revenue: ")
 	// fmt.Scan(&revenue)
-	revenue = fetchSelectedRevenue(revenue)
+	// fmt.Print("Enter expenses: ")
+	// fmt.Scan(&expenses)
+	// fmt.Print("Enter a tax rate: ")
+	// fmt.Scan(&taxRate)
 
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter a tax rate: ")
-	fmt.Scan(&taxRate)
+	revenue = getUserInput("Enter a revenue: ")
+	expenses = getUserInput("Enter expenses: ")
+	taxRate = getUserInput("Enter a tax rate: ")
 
-	ebt, profit, ratio := calculateValues(revenue, expenses)
+	ebt, profit, ratio := calculateValues(revenue, expenses, taxRate)
 
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Println(ratio)
 }
 
-func fetchSelectedRevenue(revenue float64) float64 {
-	fmt.Print("Enter a revenue: ")
-	fmt.Scan(&revenue)
-	return revenue
+func getUserInput(consoleMessage string) float64 {
+	var userInput float64
+	fmt.Print(consoleMessage)
+	fmt.Scan(&userInput)
+	return userInput
 }
 
-func calculateValues(revenue, expenses float64) (float64, float64, float64) {
+func calculateValues(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
